02aaf7985fd423f48df2c2b0542ae8e89528472e
Sebastien Lecocq <seb@vps77265.ovh.net>
2015-07-04 19:18:19 +0200

    remove all notice errors and add functionnalities to form, + log queries in DB class

---
+++ lib/ItemListField.class.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * ItemListField.class.php
+ */
+
+/**
+ * This class describes the field displayed in a smart list
+ */
+class ItemListField{
+
+	public  $field = null,
+			$class = null,
+			$title = null, 
+			$href = null,
+			$onclick = null, 
+			$style = null, 
+			$unit = null,
+			$display = null, 
+			$target = null,
+			$sort = true,
+			$search = true,
+			$independant = false,
+			$label = null,
+			$hidden = false;
+
+	/**
+	 * Constructor
+	 * @param array $param The field parameters
+	 */
+	public function __construct($param){
+		foreach($param as $key => $value){
+			$this->$key = $value;
+		}
+	}
+
+
+	/**
+	 * Get the displayed value
+	 */
+	
+}
