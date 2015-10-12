cd914097287411d5ee9600c1d9115892e407b2e4
Sebastien Lecocq <lecocq.elvyrra@gmail.com>
2015-10-13 01:10:52 +0200

    fix bug in FileSystem class : hidden files were not treated by remove and copy methods

--- lib/FileSystem.class.php
+++ lib/FileSystem.class.php
@@ -13,20 +13,36 @@ namespace Hawk;
  * @package Utils
  */
 class FileSystem{
-	const DP = DIRECTORY_SEPARATOR;
+	/**
+	 * Directory separator
+	 */
+	const DS = DIRECTORY_SEPARATOR;
 
 	const FIND_FILE_ONLY = 'file';
 	const FIND_DIR_ONLY = 'dir';
 	const FIND_ANY_TYPE = 'any';
 
 	/**
+	 * Get all files in a directory, including those beginning by '.'
+	 * @param string $dir The directory to find files into
+	 * @return array The list of files matching the pattern
+	 */
+	public static function getAllFilesInDir($dir){
+		$files = array_merge(glob($dir . self::DS . '*'), glob($dir . self::DS . '.*'));
+		return array_filter($files, function($file){
+			return basename($file) != '.' && basename($file) != '..';
+		});
+	}
+
+	/**
 	 * Equivalent to cp -r 
 	 * @param string $source The source file or directory to copy
 	 * @param string $dest The destination file or directory
+	 * @static
 	 */
 	public static function copy($source, $dest){
 		if(basename($source) == '*'){
-			foreach(glob($source) as $element){
+			foreach(self::getAllFilesInDir(dirname($source)) as $element){
 				self::copy($element, $dest);
 			}
 		}
@@ -38,20 +54,20 @@ class FileSystem{
 			if(is_file($source)){
 				// Copy a file
 				if(is_dir($dest)){
-					$dest = $dest . self::DP . basename($source);
+					$dest = $dest . self::DS . basename($source);
 				}
 				copy($source, $dest);
 			}
 			else{
 				// Copy a directory
 				$base = basename($source);
-				if(!is_dir($dest . self::DP . $base)){
-					mkdir($dest . self::DP . $base, fileperms($source), true);
+				if(!is_dir($dest . self::DS . $base)){
+					mkdir($dest . self::DS . $base, fileperms($source), true);
 				}
 
 				// Copy all files and folder under this directory
-				foreach(glob($source . self::DP . '*') as $element){				
-					self::copy($element, $dest . self::DP . $base);
+				foreach(glob($source . self::DS . '*') as $element){				
+					self::copy($element, $dest . self::DS . $base);
 				}
 			}
 		}
@@ -71,19 +87,19 @@ class FileSystem{
 		}
 		switch($type){
 			case self::FIND_FILE_ONLY :
-				$result = array_filter(glob($source . self::DP . $pattern), 'is_file');
+				$result = array_filter(glob($source . self::DS . $pattern), 'is_file');
 				break;
 
 			case self::FIND_DIR_ONLY :
-				$result = glob($source . self::DP . $pattern, GLOB_ONLYDIR);
+				$result = glob($source . self::DS . $pattern, GLOB_ONLYDIR);
 				break;
 
 			default :
-				$result = glob($source . self::DP . $pattern);
+				$result = glob($source . self::DS . $pattern);
 				break;
 		}		
 
-		$subdirs = glob($source . self::DP . '*', GLOB_ONLYDIR);
+		$subdirs = glob($source . self::DS . '*', GLOB_ONLYDIR);
 		foreach($subdirs as $dir){			
 			$result = array_merge($result, self::find($dir, $pattern, $type));
 		}
@@ -99,7 +115,7 @@ class FileSystem{
 	 */
 	public static function remove($source){	
 		if(basename($source) == '*'){
-			foreach(glob($source) as $element){
+			foreach(self::getAllFilesInDir(dirname($source)) as $element){
 				self::remove($element);
 			}
 		}
@@ -114,7 +130,7 @@ class FileSystem{
 			}
 			else{
 				// remove a directory 
-				foreach(glob($source . self::DP . '*') as $element){
+				foreach(self::getAllFilesInDir($source) as $element){
 					self::remove($element);				
 				}
 				return rmdir($source);
