1d14f1e7d8a72655094151e81809b6b8c62b784f
seb <lecocq.elvyrra@gmail.com>
2017-05-20 11:40:24 +0200

    add System lib and SystemException

---
+++ lib/exceptions/SystemException.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * SystemException.php
+ *
+ * @author  Elyrra SAS
+ * @license http://rem.mit-license.org/ MIT
+ */
+
+namespace Hawk;
+
+/**
+ * This class describes exceptions throwed by FileSystem class
+ *
+ * @package Exceptions
+ */
+class SystemException extends InternalErrorException{
+}
